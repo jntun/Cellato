7128c817cfa19c09e8df4e58aa1936ab89d8beb2
jntun <justin@jntun.com>
2023-04-08 22:11:35 -0700

    Cellato: Add interactive rule displaying using user input

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,64 @@ mod grid;
 mod cell;
 mod rule;
 
-use grid::Grid;
-use crate::cell::State;
-use crate::rule::{Config, do_wolfram_rule};
+use {
+    crate::{
+        grid::Grid,
+        cell::State,
+        grid::InitialCellConfig,
+    }
+};
+
+const WIDTH:  usize = 500;
+const EPOCHS: usize = 500;
+const START_CELL: usize = WIDTH/2;
+
+/*
+ rule: 45
+ rule: 50
+ rule: 57
+ rule: 60
+
+left off:  rule: 88
+ */
 
 fn main() {
-    let epochs = 10 as usize;
-    let mut grid = Grid::new(State::OFF, 100, 100);
+    let mut rule: u8= 0;
+    ncurses::initscr();
+    ncurses::raw();
 
-    match grid.run_wolfram_rule(epochs, 1, None) {
-        Ok(grid) => println!("{}", grid),
-        Err(e) => println!("Failed: {}", e),
+    ncurses::keypad(ncurses::stdscr(), true);
+    ncurses::noecho();
+
+
+    loop {
+        ncurses::addstr("\nto advance rule by +1, b to go back -1");
+        let ch = ncurses::getch();
+
+        match ch as u32 {
+            110 => { rule += 1; generate_grid(EPOCHS, WIDTH, rule) }, // 'n'
+            98  => { rule -= 1; generate_grid(EPOCHS, WIDTH, rule) }, // 'b'
+            101 => break, // 'e'
+            _ => {
+                ncurses::attron(ncurses::A_BOLD() | ncurses::A_BLINK());
+                ncurses::addstr("\ninvalid key, please re-enter...");
+                ncurses::attroff(ncurses::A_BOLD() | ncurses::A_BLINK());
+            },
+        }
     }
+
+    ncurses::endwin();
+}
+
+fn generate_grid(epochs: usize, width: usize, rule: u8)  {
+     match Grid::new(width, epochs).run_wolfram_rule(rule, Some(vec![(State::ON, START_CELL)])) {
+         Ok(grid) => {
+             ncurses::clear();
+             ncurses::attron(ncurses::A_BOLD() | ncurses::A_BLINK());
+             ncurses::addstr(format!("\t\t\t\t\trule: {}\n\n", rule).as_str());
+             ncurses::addstr(format!("{}\n", grid).as_str());
+             ncurses::attroff(ncurses::A_BOLD() | ncurses::A_BLINK());
+         },
+         Err(e) => println!("Failed rule {}: {}", rule, e),
+     }
 }
