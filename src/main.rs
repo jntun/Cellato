dfa41eb57bcb41d7bb107e504660d7d48cab99a4
jntun <justin@jntun.com>
2023-04-10 16:44:28 -0700

    Cellato: Implement CLI flag options for easier user generation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod grid;
 mod cell;
 mod rule;
 
+use std::convert::Infallible;
+use std::process::{ExitCode, Termination};
 use {
     crate::{
         grid::Grid,
@@ -9,10 +11,13 @@ use {
         grid::InitialCellConfig,
     }
 };
+use crate::CellatoResult::InvalidCommand;
+use crate::grid::GridError;
 
-const WIDTH:  usize = 500;
-const EPOCHS: usize = 500;
+const WIDTH:  usize = 100;
+const EPOCHS: usize = 210;
 const START_CELL: usize = WIDTH/2;
+const RULE: u8 = 1;
 
 /*
  rule: 45
@@ -23,22 +28,98 @@ const START_CELL: usize = WIDTH/2;
 left off:  rule: 88
  */
 
-fn main() {
-    let mut rule: u8= 0;
+#[repr(u8)]
+pub enum CellatoResult {
+    Success = 0,
+    InvalidCommand = 64,
+    GridError = 70,
+}
+
+impl Termination for CellatoResult {
+    fn report(self) -> ExitCode {
+        ExitCode::from(self as u8)
+    }
+}
+
+fn main() -> CellatoResult {
+    let mut interactive = false;
+    let mut epochs: usize     = EPOCHS;
+    let mut width:  usize     = WIDTH;
+    let mut rule:   u8        = RULE;
+    let mut start_config: InitialCellConfig = Vec::new();
+
+    for (argc, arg) in std::env::args().enumerate() {
+        match arg.as_str() {
+            "-e" => {
+                let Some(epoch_arg) = std::env::args().nth(argc+1) else {
+                    println!("Internal error processing '-e' flag");
+                    return CellatoResult::InvalidCommand
+                };
+                epochs = epoch_arg.parse::<usize>().unwrap();
+            },
+            "-w" => {
+                let Some(width_arg) = std::env::args().nth(argc+1) else {
+                    println!("Internal error processing '-w' flag");
+                    return CellatoResult::InvalidCommand
+                };
+                match width_arg.parse::<u64>() {
+                    Ok(width_arg_i) => width = width_arg_i as usize,
+                    Err(e) => {
+                        println!("Invalid '-w' flag input, want a positive 64-bit integer. Got: {}", width_arg);
+                        return InvalidCommand
+                    }
+                }
+            },
+            "-r" => {
+                let Some(rule_arg) = std::env::args().nth(argc+1) else {
+                    println!("Provided '-w' flag without matching value.");
+                    return InvalidCommand
+                };
+                match rule_arg.parse::<u8>() {
+                    Ok(rule_arg_u8) => rule = rule_arg_u8,
+                    Err(e) => {
+                        println!("Invalid '-r' flag input, want a positive 8-bit integer. Got: {}", rule_arg);
+                        return InvalidCommand
+                    }
+                }
+            },
+            "-i" => interactive = true,
+            _ => (),
+        }
+    }
+    start_config.push((State::ON, width/2));
+    if interactive {
+        if let Err(e) = interactive_grid(epochs, width, rule, start_config) {
+            println!("Failed interactive grid: {}", e);
+            ncurses::attroff(ncurses::A_BOLD() | ncurses::A_BLINK());
+            ncurses::endwin();
+            return CellatoResult::GridError
+        }
+    } else {
+        if let Err(e) = generate_static_grid(epochs, width, rule, start_config) {
+            println!("Failed to make static grid: {}", e);
+            return CellatoResult::GridError
+        }
+    }
+    CellatoResult::Success
+}
+
+fn interactive_grid(epochs: usize, width: usize, rule: u8, start_config: InitialCellConfig) -> Result<(), GridError>{
+    let mut rule: u8 = rule;
+
     ncurses::initscr();
     ncurses::raw();
 
     ncurses::keypad(ncurses::stdscr(), true);
     ncurses::noecho();
 
-
     loop {
         ncurses::addstr("\nto advance rule by +1, b to go back -1");
         let ch = ncurses::getch();
 
         match ch as u32 {
-            110 => { rule += 1; generate_grid(EPOCHS, WIDTH, rule) }, // 'n'
-            98  => { rule -= 1; generate_grid(EPOCHS, WIDTH, rule) }, // 'b'
+            110 => { rule += 1; if let Err(e) = generate_interactive_grid(epochs, width, rule, start_config.clone()) { return Err(e) }}, // 'n'
+            98  => { rule -= 1; if let Err(e) = generate_interactive_grid(epochs, width, rule, start_config.clone()) { return Err(e) }}, // 'b'
             101 => break, // 'e'
             _ => {
                 ncurses::attron(ncurses::A_BOLD() | ncurses::A_BLINK());
@@ -49,10 +130,11 @@ fn main() {
     }
 
     ncurses::endwin();
+    Ok(())
 }
 
-fn generate_grid(epochs: usize, width: usize, rule: u8)  {
-     match Grid::new(width, epochs).run_wolfram_rule(rule, Some(vec![(State::ON, START_CELL)])) {
+fn generate_interactive_grid(epochs: usize, width: usize, rule: u8, start_config: InitialCellConfig) -> Result<(), GridError>{
+     match Grid::new(width, epochs).run_wolfram_rule(rule, Some(start_config)) {
          Ok(grid) => {
              ncurses::clear();
              ncurses::attron(ncurses::A_BOLD() | ncurses::A_BLINK());
@@ -60,6 +142,15 @@ fn generate_grid(epochs: usize, width: usize, rule: u8)  {
              ncurses::addstr(format!("{}\n", grid).as_str());
              ncurses::attroff(ncurses::A_BOLD() | ncurses::A_BLINK());
          },
-         Err(e) => println!("Failed rule {}: {}", rule, e),
+         Err(e) => return Err(e),
      }
+    Ok(())
+}
+
+fn generate_static_grid(epochs: usize, width: usize, rule: u8, start_config: InitialCellConfig) -> Result<(), GridError> {
+    match Grid::new(width, epochs).run_wolfram_rule(rule, Some(start_config)) {
+        Ok(grid) => println!("\t\t\trule: {}\n{}", rule, grid),
+        Err(e) => return Err(e),
+    }
+    Ok(())
 }
